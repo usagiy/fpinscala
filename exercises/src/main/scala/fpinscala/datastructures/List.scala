19470c308983fef0297161e2956d63cafcf06533
ZLATKOPC\zlatko <zlatko.roncevic@gmail.com>
2017-01-01 17:49:25 +0100

    Exercises

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,5 +1,6 @@
 package fpinscala.datastructures
 
+
 sealed trait List[+A] // `List` data type, parameterized on a type, `A`
 case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
 /* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
@@ -50,19 +51,100 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  //Exercise  
+    
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, t) => t    
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => sys.error("setHead on empty list")
+    case Cons(_,t) => Cons(h,t)
+  }
+    
+  def drop[A](l: List[A], n: Int): List[A] = {
+    
+    @annotation.tailrec
+    def go(n: Int, index: Int, l: List[A]): List[A] = {
+      if (index >= n) l
+      else go(n,index+1,tail(l))
+    }
+    go(n,0,l)    
+  }
+  
+  def drop1[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_,t) => drop(t, n-1)
+    }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+    
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Cons(h,t) if f(h) => dropWhile(t, f)
+      case _ => l
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("init of empty list")
+      case Cons(_,Nil) => Nil
+      case Cons(h,t) => Cons(h,init(t))
+    }
+  
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  def length[A](l: List[A]): Int = {   
+    @annotation.tailrec
+    def go(n: Int, l: List[A]): Int = {
+      l  match {
+        case Nil => sys.error("init of empty list")
+        case Cons(_,Nil) => n+1
+        case Cons(h,t) => go(n+1,t)
+      }
+    }
+    go(0,l)    
+  }
+  
+   def lenth2[A](l: List[A]) =
+    foldRight(l, 0)((_,acc) => acc + 1)
 
-  def length[A](l: List[A]): Int = sys.error("todo")
-
+  
+  
+  
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+    
+  
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  def map[A,B](l: List[A])(f: A => B): List[B] =
+    foldRight(l, Nil:List[B])((h,t) => Cons(f(h),t))
+  
+}
+
+
+object testList {
+  def main(args: Array[String]): Unit = {
+    println("List ")
+    val l = List(5,6,7,8);
+    println("List pattern: %d".format(List.x));
+    println("List tail:");
+    println(List.tail(l))
+    println(List.setHead(l, 4))
+    println(List.drop(l, 2))
+    println(List.init(l))
+    println(List.length(l))
+    
+    
+    
+    /*
+    var a = Array(1, 2, 3,4)
+    println(a.toString())
+    println ("Is sorted: %b".format(isSorted(a,  (a: Int,b: Int) => a > b))) 
+    var b = Array(1, 2, 3,5,4)
+    println ("Is sorted: %b".format(isSorted(b,  (a: Int,b: Int) => a > b)))
+    * */ 
+    
+  }
 }
--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -176,13 +176,14 @@ object PolymorphicFunctions {
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    a => b => f(a, b)
+
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+    (a,b) => f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -197,7 +198,7 @@ object PolymorphicFunctions {
   // Exercise 5: Implement `compose`
 
   def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+    (a) => f(g(a))  
   
 
   // test implementation of `fib`
